d1cb5d65ee25096c7f4c4fd16718258ab1497bd6
geetaramya <45258571+geetaramya@users.noreply.github.com>
2019-06-04 20:33:37 +0530

    addedsignup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var monk=require('monk');
+var db=monk('localhost:27017/aditya');
+var collection=db.get('signup');
+console.log('connected');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.post('/signup',function(req,res){
+	var username=req.body.name;
+	console.log(username);
+	var email=req.body.email;
+	console.log(email);
+	var password=req.body.password;
+	console.log(password);
+	var confirmpassword=req.body.password2;
+	console.log(confirmpassword);
+	collection.insert({"username":req.body.name,"email":req.body.email,"password":req.body.password,"confirmpassword":req.body.name});
+	res.redirect('/');
 });
 
 module.exports = router;
